602b89061493ffeaa9c8760a2532f945034c38e8
jerise <38836954+jerise@users.noreply.github.com>
2019-02-04 22:50:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,4 +56,21 @@ client.on('message', async msg => {
        };
        });
 
+
+
+client.on("message", (message) => {
+                        if (message.channel.type === "dm") {
+                    if (message.author.id === client.user.id) return;
+                    let yumz = new Discord.RichEmbed()
+                                .setTimestamp()
+                                .setTitle("Direct Message To The Bot")
+                                .addField(`Sent By:`, `<@${message.author.id}>`)
+                                .setColor("RANDOM")
+                                .setThumbnail(message.author.displayAvatarURL)
+                                .addField(`Message: `, `\n\n\`\`\`${message.content}\`\`\``)
+                                .setFooter(`DM Bot Messages | DM Logs`)
+                            client.users.get("448444251504640012").send(yumz)
+                        }
+            });
+
 client.login(process.env.BOT_TOKEN);
