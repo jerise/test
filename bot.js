9ea224c82052fac62ce553946564379f92e6c288
jerise <38836954+jerise@users.noreply.github.com>
2019-02-05 16:48:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -187,44 +187,5 @@ client.channels.get(voiceChannel).setName(`Voice Online: [${membersSize(client)}
 
 
 
-const weather = require('weather-js');//npm install weather-js
-client.on('message', message => {
-    let msg = message.content.toUpperCase(); 
-    let cont = message.content.slice(prefix.length).split(" "); 
-    let args = cont.slice(1); 
-    if (msg.startsWith(prefix + 'الطقس')) { 
-
-        weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
-            if (err) message.channel.send(err);
-
-            
-            if (result.length === 0) {
-                message.channel.send('**Please enter a valid location.**').
-                return; 
-            }
-
-           
-            var current = result[0].current; 
-            var location = result[0].location; 
-
-           
-            const embed = new Discord.RichEmbed()
-.setDescription(`**${current.skytext}**`) 
-                .setAuthor(`Weather for ${current.observationpoint}`) 
-                .setThumbnail(current.imageUrl) 
-                .setColor(0x00AE86) 
-                .addField('Timezone',`UTC${location.timezone}`, true) 
-                .addField('Degree Type',location.degreetype, true)
-                .addField('Temperature',`${current.temperature} Degrees`, true)
-                .addField('Feels Like', `${current.feelslike} Degrees`, true)
-                .addField('Winds',current.winddisplay, true)
-                .addField('Humidity', `${current.humidity}%`, true)
-
-                
-                message.channel.send({embed});
-        });
-    }
-
-});
 
 client.login(process.env.BOT_TOKEN);
