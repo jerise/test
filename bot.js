4f723112365a6c5d30b3b8493e577069d3fa3398
jerise <38836954+jerise@users.noreply.github.com>
2019-02-05 16:43:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -168,4 +168,21 @@ client.on('message', async message => {
            })
           }
       });
+
+
+const voiceChannel = '542354384500162590'; 
+
+const membersSize = (client) => {
+ return client.channels.filter(c => c.type === "voice").map(c => c.members.size).reduce((a,b) => {return a + b}, 0);
+}
+
+
+client.on('ready', () => {
+client.channels.get(voiceChannel).setName(`Voice Online: [${membersSize(client)}]`)
+}); 
+
+client.on('voiceStateUpdate', () => {
+client.channels.get(voiceChannel).setName(`Voice Online: [${membersSize(client)}]`)
+})
+
 client.login(process.env.BOT_TOKEN);
