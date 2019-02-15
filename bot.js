1aecc6460ca2f4db26bb5bf9285b08774e49953b
jerise <38836954+jerise@users.noreply.github.com>
2019-02-15 23:04:09 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,96 @@
+   const Discord = require('discord.js');
+const { Client, Util } = require('discord.js');
+const client = new Discord.Client();
+const { PREFIX, GOOGLE_API_KEY } = require('./config');
+const prefix = '+'
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube(GOOGLE_API_KEY);
+const queue = new Map();
+const moment = require('moment');
+const fs = require("fs");
+const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
+const coolDown = new Set();
+const db = require('quick.db')
+const ms = require('ms')
+const bot = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame('| +help |  | +inv |')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+   
+   
+    client.on('message' , najzx => {
+          var prefix = "+";
+          if(najzx.author.bot) return;
+         
+          if(najzx.content.startsWith(prefix + "rolebc")) {
+            if (!najzx.member.hasPermission("ADMINISTRATOR"))  return;
+            let args = najzx.content.split(" ").slice(1);
+         
+            if(!args[0]) {
+              najzx.channel.send("قم بمنشنة الرتبة | +rolebc @everyone الرساله")
+                return;
+            }
+            if(!args[1]) {
+              najzx.channel.send("قم بكتابة الرسالة | +rolebc @everyone الرساله")
+                return;
+            }
+         
+              if(args[0] == "@everyone") {
+                najzx.channel.send(`لقد تم ارسال هذه الرسالة الى ${najzx.guild.memberCount} اعضاء`)
+                najzx.guild.members.forEach(m => {
+                  m.send(
+                  "**" + "السيرفر :" + "\n" +
+                  `${najzx.guild.name}` + "\n" +
+                  "المرسل :" + "\n" +
+                  `${najzx.author.tag}` + "\n" +
+                  "الرسالة :" + "\n" +
+                  `${args[1]}` + "**"
+                  )
+                })
+                return;
+              }
+         
+                  var role = najzx.mentions.roles.first();
+                    if(!role) {
+                      najzx.reply("لا توجد رتبة بهذا الاسم")
+                        return;
+                    }
+                najzx.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+                  n.send(
+                  "**" + "السيرفر :" + "\n" +
+                  `${najzx.guild.name}` + "\n" +
+                  "المرسل :" + "\n" +
+                  `${najzx.author.tag}` + "\n" +
+                  "الرسالة :" + "\n" +
+                  `${args[1]}` + "**"
+                  )
+                })
+                najzx.channel.send(`لقد تم ارسال هذه الرسالة الى ${najzx.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
+            }
+        });
+   
+client.login(process.env.BOT_TOKEN);
