b1c46d75018bd460c903f9b0d8aec74abb714c57
jerise <38836954+jerise@users.noreply.github.com>
2019-02-16 21:16:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,32 @@ client.user.setGame('| +help |  | +inv |')
 });
    
    
-client.on('message', message => require('./commands/bc/bcrole.js')(client, message));
+client.on('message' , message => {
+      var prefix = "+";
+      if(message.author.bot) return;
+     
+      if(message.content.startsWith(prefix + "bcrole")) {
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        let args = message.content.split(" ").slice(2);
+     var codes = args.join(' ')
+       
+        if(!codes) {
+          message.channel.send("قم بكتابة الرسالة | `+rolebc role message`")
+            return;
+        }
+     
+     
+              var role = message.mentions.roles.first();
+                if(!role) {
+                  message.reply("لا توجد رتبة بهذا الاسم")
+                    return;
+                }
+            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+              n.send(`${codes}`)
+            })
+            message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
+        }
+    });
 
 
 
